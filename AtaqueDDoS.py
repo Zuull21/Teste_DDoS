bcbfe5d7532af5510a7375020d34a093a967ca58
Julio Cesar <125297471+Zuull21@users.noreply.github.com>
2023-12-02 01:10:06 +0000

    Atualiza;'ao Script de monitoramento e de ataque

--- AtaqueDDoS.py
+++ AtaqueDDoS.py
@@ -1,53 +1,67 @@
+import pymssql
 from scapy.all import sniff, IP
 from datetime import datetime, timedelta
-import threading
-import time
+
+# Conectar ao banco de dados
+sql_server_cnx = pymssql.connect(
+    server='18.204.118.27',
+    database='SecureATM',
+    user='sa',
+    password='Secure2023'
+)
+cursor_sql_server = sql_server_cnx.cursor()
 
 class TrafficAnalyzer:
     def __init__(self):
-        self.ip_traffic = {}  # Dicionário para armazenar o tráfego por IP
-        self.alert_threshold = 1.5  # 50% acima da média
-        self.running = True  # Variável para controlar a execução do script
-        self.trafego_normal = 0  # Variável para indicar tráfego normal
-        self.alteracoes_identificadas = 0  # Variável para indicar alterações no tráfego
+        self.ip_traffic = {}
+        self.alert_threshold = 5000
+        self.alerted_ips = set()
+        self.running = True
 
     def packet_callback(self, packet):
         if IP in packet:
             src_ip = packet[IP].src
-            dst_ip = packet[IP].dst
 
-            # Atualizar o tráfego por IP
-            self.ip_traffic[src_ip] = self.ip_traffic.get(src_ip, 0) + 1
+            if src_ip not in self.ip_traffic:
+                self.ip_traffic[src_ip] = []
 
-    def calculate_traffic_stats(self):
-        # Calcular a média de pacotes por minuto para cada IP
-        current_time = datetime.now()
-        minute_ago = current_time - timedelta(minutes=1)
+            timestamp = datetime.now()
+            self.ip_traffic[src_ip].append(timestamp)
 
-        for ip, count_time in list(self.ip_traffic.items()):
-            if count_time < minute_ago:
-                del self.ip_traffic[ip]
+            # Verificar se atingiu o limiar de alerta
+            if len(self.ip_traffic[src_ip]) >= self.alert_threshold and src_ip not in self.alerted_ips:
+                self.alerted_ips.add(src_ip)
+                self.stop_sniffing()
+                self.insert_ddos_alert(src_ip, len(self.ip_traffic[src_ip]))
             else:
-                self.ip_traffic[ip] = sum(1 for t in count_time if t >= minute_ago)
+                self.insert_network_data(src_ip, timestamp)
 
-    def check_for_alerts(self):
-        # Verificar se há alertas com base na média por minuto
-        alerts = []
-        for ip, count in self.ip_traffic.items():
-            average = count / 1  # 1 minuto para simplificar
-            if count > 0 and self.ip_traffic[ip] > self.alert_threshold * average:
-                alerts.append(ip)
+    def insert_network_data(self, ip, timestamp):
+        try:
+            cursor_sql_server.execute("INSERT INTO rede (IP, data_hora, pacotesEnviados) VALUES (%s, %s, 1)", (ip, timestamp))
+            sql_server_cnx.commit()
 
-        return alerts
+        except pymssql.Error as e:
+            print(f"Erro ao inserir dados na tabela rede: {e}")
+
+    def insert_ddos_alert(self, ip, packet_count):
+        try:
+            cursor_sql_server.execute("INSERT INTO DDoS (IPAtq, qtdPacotesAtq) VALUES (%s, %s)", (ip, packet_count))
+            sql_server_cnx.commit()
+
+        except pymssql.Error as e:
+            print(f"Erro ao inserir dados na tabela DDoS: {e}")
 
     def start_sniffing(self, interface):
         print("Iniciando análise de tráfego...")
-        sniff(iface=interface, prn=self.packet_callback, store=0)
-        print("Análise de tráfego encerrada.")
+        sniff(iface=interface, prn=self.packet_callback, store=0, stop_filter=lambda x: not self.running)
 
     def stop_sniffing(self):
         print("Parando análise de tráfego...")
         self.running = False
+
+        # Fechar a conexão com o banco de dados
+        sql_server_cnx.close()
 
 # Substitua "enp0s3" pelo nome da sua interface de rede
 interface = "enp0s3"
@@ -55,33 +69,11 @@ interface = "enp0s3"
 # Criar instância do analisador de tráfego
 analyzer = TrafficAnalyzer()
 
-# Iniciar a captura de pacotes em uma thread separada
-sniff_thread = threading.Thread(target=analyzer.start_sniffing, args=(interface,))
-sniff_thread.start()
-
 try:
-    while analyzer.running:
-        # Executar verificações e imprimir mensagens
-        analyzer.calculate_traffic_stats()
-        alerts = analyzer.check_for_alerts()
-
-        if not alerts:
-            analyzer.trafego_normal = 1
-            analyzer.alteracoes_identificadas = 0
-            print("Tráfego normal.")
-        else:
-            analyzer.trafego_normal = 0
-            analyzer.alteracoes_identificadas = 1
-            print(f"Alterações no tráfego identificadas. IPs com tráfego elevado: {', '.join(alerts)}")
-
-        # Aguardar antes de realizar a próxima verificação
-        time.sleep(60)
+    # Iniciar a captura de pacotes
+    analyzer.start_sniffing(interface)
 
 except KeyboardInterrupt:
     # Se o usuário pressionar Ctrl+C, interromper o script
     pass
 
-finally:
-    # Parar a captura de pacotes e esperar que a thread termine
-    analyzer.stop_sniffing()
-    sniff_thread.join()
--- SimulacaoAtkDDoS.py
+++ SimulacaoAtkDDoS.py
@@ -2,15 +2,16 @@ import os
 import platform
 import time
 
-def send_ping(ip_address, count=5000, interval=0.01):
+def send_ping(ip_address, count=5000, duration=30):
     """
-    Envia pings para um endereço IP específico.
+    Envia pings para um endereço IP específico por um determinado período.
 
     Parâmetros:
     - ip_address: Endereço IP de destino.
     - count: Número de pings a serem enviados.
-    - interval: Intervalo entre os pings em segundos.
+    - duration: Duração em segundos para enviar os pings.
     """
+    interval = duration / count
     if platform.system().lower() == "windows":
         command = f"ping -n {count} -i {interval} {ip_address}"
     else:
@@ -21,8 +22,8 @@ def send_ping(ip_address, count=5000, interval=0.01):
 # Substitua "192.168.1.1" pelo IP de destino desejado
 ip_destino = "131.72.61.69"
 
-# Envie pings repetidamente para o IP de destino com intervalo de 0.01 segundos
-send_ping(ip_destino, count=5000, interval=0.01)
+# Envie pings repetidamente para o IP de destino por 30 segundos
+send_ping(ip_destino, count=5000, duration=30)
 
 input("Pressione Enter para sair")
 
